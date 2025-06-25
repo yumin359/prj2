247bd6dbed12cc88f60205872f6b20b02c31bdc7
yumin359 <yumin359@gmail.com>
2025-06-25 09:18:28 +0900

    first commit

---
+++ src/main/java/com/example/prj2/Prj2Application.java
@@ -0,0 +1,13 @@
+package com.example.prj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prj2/Prj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
