99e620b83e3ce523dde3f4040146324fa4cfb661
yumin359 <yumin359@gmail.com>
2025-06-26 16:16:41 +0900

    나중

--- src/main/java/com/example/prj2/home/controller/HomeController.java
+++ src/main/java/com/example/prj2/home/controller/HomeController.java
@@ -4,7 +4,6 @@ import com.example.prj2.home.service.HomeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,5 +14,15 @@ public class HomeController {
     public String home() {
         return "layout/home"; // templates/layout/home.html
     }
-}
 
+//    // 얘네는 나중에 익숙해지면 하는 거로 하구여
+//    @GetMapping("login")
+//    public String login() {
+//        return "layout/login"; // 얘도 layout 이름 나중에 바꿀듯
+//    }
+//
+//    @GetMapping("logout")
+//    public String logout() {
+//        return "redirect:/--";
+//    }
+}
