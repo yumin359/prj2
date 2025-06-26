7151de12423b8cbe0d58196309980416636e3b3f
yumin359 <yumin359@gmail.com>
2025-06-26 16:52:34 +0900

    외래키 제약사항 추가

--- sql/prj2.sql
+++ sql/prj2.sql
@@ -27,4 +27,23 @@ CREATE TABLE member
     birth_date DATE        NOT NULL,
     info       VARCHAR(1000)
 );
-DROP TABLE member;
+
+# 회원만 글을 작성할 수 있고,
+# 자기가 쓴글은 자신만 수정/삭제 할 수 있으므로
+# board.writer를 member.id로 수정해야함
+# -> 외래키 제약사항 추가
+# 한명의 회원은 여러개의 게시물을 작성할 수 있으므로 member:board=1:N관계
+# 즉 board 테이블에 writer를 FK로 설정 references member(id)
+
+# 위와 같은 것이 잘 되는지 확인하기 위해 작성자를 cho(member.id) 또는 min(member.id) 으로 바꿔줌
+
+UPDATE board
+SET writer = 'cho'
+WHERE id % 2 = 1;
+UPDATE board
+SET writer = 'min'
+WHERE id % 2 = 0;
+
+# 외래키 제약사항 추가
+ALTER TABLE board
+    ADD FOREIGN KEY (writer) REFERENCES member (id);
